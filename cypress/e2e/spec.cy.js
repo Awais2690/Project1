ad2ec8cf27c232d5ce7fbd5fd90b387579c69fb1
Awais2690 <107214630+Awais2690@users.noreply.github.com>
2022-06-10 05:22:04 -0700

    Initial files

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,19 @@
+const cypress = require("cypress")
+
+describe('spec.cy.js', () => {
+  it('should visit', () => {
+    cy.visit('https://test-2.ampliflow.com/login')
+    // cy.get('input.gLFyf').type('Automation Step by Step{enter}')
+    // // cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l').click()
+    //   // cy.get('.dmenKe > .cIkxbf > .usJj9c > h3 > .l').click()
+    //   cy.contains('videos').click()
+
+  })
+
+
+})
+
+// it('login test', function(){
+//   cy.visit('https://www.saucedemo.com/')
+  
+//   })
